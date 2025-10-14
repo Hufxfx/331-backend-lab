b729ba640a2e0c5ed7439ee4e8e7a788df004a49
Chamor Lahuna <Chamor_l@cmu.ac.th>
2025-10-14 16:17:14 +0700

    Lab09[6.1]: add the organizer

--- src/main/java/se331/lab/dao/OrganizerDao.java
+++ src/main/java/se331/lab/dao/OrganizerDao.java
@@ -1,12 +1,10 @@
-//package se331.lab.dao;
-//
-//import org.springframework.data.domain.Page;
-//import se331.lab.entity.Organizer;
-//
-//
-//public interface OrganizerDao {
-//    Integer getOrganizerSize();
-//    Page<Organizer> getOrganizers(Integer pageSize, Integer page);
-//    Organizer getOrganizer(Long id);
-//    Organizer save (Organizer organizer);
-//}
+package se331.lab.dao;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import se331.lab.entity.Organizer;
+
+
+public interface OrganizerDao {
+    Page<Organizer> getOrganizer(Pageable pageRequest);
+}
--- src/main/java/se331/lab/service/OrganizerService.java
+++ src/main/java/se331/lab/service/OrganizerService.java
@@ -1,14 +1,11 @@
-//package se331.lab.service;
-//
-//import org.springframework.data.domain.Page;
-//import se331.lab.entity.Event;
-//import se331.lab.entity.Organizer;
-//
-//import java.util.List;
-//
-//public interface OrganizerService {
-//    Integer getOrganizerSize();
-//    Page<Organizer> getOrganizers(Integer pageSize, Integer page);
-//    Organizer getOrganizer(Long id);
-//    Organizer save(Organizer organizer);
-//}
+package se331.lab.service;
+
+import org.springframework.data.domain.Page;
+import se331.lab.entity.Organizer;
+
+import java.util.List;
+
+public interface OrganizerService {
+    List<Organizer> getAllOrganizer();
+    Page<Organizer> getOrganizer(Integer page, Integer pageSize);
+}
